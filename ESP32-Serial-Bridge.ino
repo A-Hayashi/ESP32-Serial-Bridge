da6307cabdd4c8bad2a4dfd58c9123685fd92b17
akionori <a.hayashi0113@gmail.com>
2022-07-02 12:48:13 +0900

    タスクに置き換え完了

--- ESP32-Serial-Bridge.ino
+++ ESP32-Serial-Bridge.ino
@@ -1,58 +1,8 @@
-/*
-    Copyright(C) 2018 by Yukiya Ishioka
-*/
-
-#include <freertos/FreeRTOS.h>    /* FreeRTOSを用いるためのヘッダファイル */
-
-byte a = 0;
-
-void  task1( void *param )
-{
-  Serial.begin(115200);
-  Serial.print("setup()\n");
-  while ( 1 ) {
-    Serial.println(a);
-    vTaskDelay(100);
-  }
-}
-
-void  task2( void *param )
-{
-  while ( 1 ) {
-    a++;
-    vTaskDelay(1000);
-  }
-}
-
-//void setup()
-//{
-//  /* create task */
-//  xTaskCreatePinnedToCore( task1,   /* タスクの入口となる関数名 */
-//                           "TASK1", /* タスクの名称 */
-//                           0x800,   /* スタックサイズ */
-//                           NULL,    /* パラメータのポインタ */
-//                           1,       /* プライオリティ */
-//                           NULL,    /* ハンドル構造体のポインタ */
-//                           0 );     /* 割り当てるコア (0/1) */
-//
-//  xTaskCreatePinnedToCore( task2,
-//                           "TASK2",
-//                           0x800,
-//                           NULL,
-//                           2,
-//                           NULL,
-//                           0 );
-//}
-//
-//void loop()
-//{
-//
-//}
+#include <freertos/FreeRTOS.h>
 
 #include <esp_wifi.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-
 
 #define MODE_STA // phone connects directly to ESP
 
@@ -61,41 +11,115 @@ const char *ssid = "aterm-82f586-gw";  // You will connect your phone to this Ac
 const char *pw = "4u2V58F#"; // and this is the password
 IPAddress ip(192, 168, 10, 30); // From RoboRemo app, connect to this IP
 IPAddress netmask(255, 255, 255, 0);
-
-/*************************  COM Port 0 *******************************/
-#define UART_BAUD0 19200            // Baudrate UART0
-#define SERIAL_PARAM0 SERIAL_8N1    // Data/Parity/Stop UART0
-#define SERIAL0_RXPIN 16            // receive Pin UART0
-#define SERIAL0_TXPIN 17
-
-
-
-// transmit Pin UART0
-#define SERIAL0_TCP_PORT 8880       // Wifi Port UART0
-
-
 #define bufferSize 1024
 
-HardwareSerial Serial_one(1);
+class Bridge
+{
+  public:
+    Bridge(int uartNo, int baudRate, int serialParam, int rxdPin, int txdPin, int port);
+    static void runner(void *params);
+    void start();
+    void loop();
+  private:
+    HardwareSerial serial;
+    int port;
 
-WiFiServer server(SERIAL0_TCP_PORT);
-WiFiClient TCPClient;
+    WiFiServer server;
+    WiFiClient TCPClient;
 
-uint8_t buf1[bufferSize];
-uint16_t i1 = 0;
+    uint8_t buf1[bufferSize];
+    uint16_t i1;
 
-uint8_t buf2[bufferSize];
-uint16_t i2 = 0;
+    uint8_t buf2[bufferSize];
+    uint16_t i2;
+};
 
-uint8_t BTbuf[bufferSize];
-uint16_t iBT = 0;
+Bridge::Bridge(int uartNo, int baudRate, int serialParam, int rxdPin, int txdPin, int port):
+  serial(uartNo)
+{
+  serial.begin(baudRate, serialParam, rxdPin, txdPin);
+  this->port = port;
+}
+
+void Bridge::start(void)
+{
+  /* create task */
+  xTaskCreatePinnedToCore( this->runner,   /* タスクの入口となる関数名 */
+                           "TASK1", /* タスクの名称 */
+                           0x800,   /* スタックサイズ */
+                           this,    /* パラメータのポインタ */
+                           1,       /* プライオリティ */
+                           NULL,    /* ハンドル構造体のポインタ */
+                           0 );     /* 割り当てるコア (0/1) */
+
+}
+
+void Bridge::runner(void *params)
+{
+  Bridge* ref = static_cast<Bridge *>(params);
+  ref->loop();
+}
+
+void Bridge::loop()
+{
+  server.begin(port); // start TCP server
+  server.setNoDelay(true);
+
+  while (1) {
+    if (server.hasClient())
+    {
+      //find free/disconnected spot
+      if (!TCPClient || !TCPClient.connected()) {
+        if (TCPClient) {
+          TCPClient.stop();
+        }
+        TCPClient = server.available();
+      }
+      //no free/disconnected spot so reject
+      WiFiClient TmpserverClient = server.available();
+      TmpserverClient.stop();
+    }
+
+    if (TCPClient)
+    {
+      while (TCPClient.available())
+      {
+        buf1[i1] = TCPClient.read(); // read char from client (LK8000 app)
+        if (i1 < bufferSize - 1) {
+          i1++;
+        }
+      }
+
+      serial.write(buf1, i1); // now send to UART(num):
+      i1 = 0;
+    }
+
+    if (serial.available())
+    {
+      while (serial.available())
+      {
+        buf2[i2] = serial.read(); // read char from UART(num)
+        if (i2 < bufferSize - 1) {
+          i2++;
+        }
+      }
+      // now send to WiFi:
+      if (TCPClient) {
+        TCPClient.write(buf2, i2);
+      }
+      i2 = 0;
+    }
+    vTaskDelay(1);
+  }
+
+}
+
+Bridge b(1, 19200, SERIAL_8N1, 16, 17, 8880);
 
 void setup() {
-
   delay(500);
+  Serial.begin(115200);
 
-  Serial_one.begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
-  Serial.begin(115200, SERIAL_8N1, 3, 1);
 #ifdef MODE_AP
   //AP mode (phone connects directly to ESP) (no router)
   WiFi.mode(WIFI_AP);
@@ -116,57 +140,11 @@ void setup() {
   Serial.println(WiFi.localIP());
 #endif
 
-  server.begin(); // start TCP server
-  server.setNoDelay(true);
-
   esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
+
+  b.start();
 }
 
+void loop() {
 
-
-void loop()
-{
-  if (server.hasClient())
-  {
-    //find free/disconnected spot
-    if (!TCPClient || !TCPClient.connected()) {
-      if (TCPClient) {
-        TCPClient.stop();
-      }
-      TCPClient = server.available();
-    }
-    //no free/disconnected spot so reject
-    WiFiClient TmpserverClient = server.available();
-    TmpserverClient.stop();
-  }
-
-  if (TCPClient)
-  {
-    while (TCPClient.available())
-    {
-      buf1[i1] = TCPClient.read(); // read char from client (LK8000 app)
-      if (i1 < bufferSize - 1) {
-        i1++;
-      }
-    }
-
-    Serial_one.write(buf1, i1); // now send to UART(num):
-    i1 = 0;
-  }
-
-  if (Serial_one.available())
-  {
-    while (Serial_one.available())
-    {
-      buf2[i2] = Serial_one.read(); // read char from UART(num)
-      if (i2 < bufferSize - 1) {
-        i2++;
-      }
-    }
-    // now send to WiFi:
-    if (TCPClient) {
-      TCPClient.write(buf2, i2);
-    }
-    i2 = 0;
-  }
 }
